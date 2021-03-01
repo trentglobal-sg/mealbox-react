ed299b1df89a3c95fe5dde8fe55a5d8e74e85347
yongsannnn <yong_s@live.com>
2021-03-01 08:20:12 +0000

    Added state and form to add new comments

--- mealbox/src/AddComments.js
+++ mealbox/src/AddComments.js
@@ -6,23 +6,30 @@ const baseURL = "https://3001-bronze-barnacle-pdcp8mf3.ws-us03.gitpod.io"
 
 export default class AddComments extends React.Component {
     state = {
-        comments: []
+        commentsList: [],
+        comments: "",
+        username: "",
+        recipe_name: "",
+        user_id: "",
+        recipe_id: "",
+
     }
 
     // Lifecycle Import
     async componentDidMount() {
         let response = await axios.get(baseURL + "/comments");
         this.setState({
-            comments: response.data
+            commentsList: response.data
         })
     }
 
     renderCommentsList = () => {
         let list = [];
-        for (let l of this.state.comments) {
+        for (let l of this.state.commentsList) {
             list.push(
                 <div key={l._id}>
                     <h5>{l.recipe_name}</h5>
+                    <p>ID:{l.recipe_id}</p>
                     <p>{l.comments}</p>
                     <p>By: {l.username}</p>
                 </div>
@@ -31,9 +38,54 @@ export default class AddComments extends React.Component {
         return list
     }
 
+    updateField = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    add = (e) => {
+        // let newComment = {
+
+        // }
+    }
+
     render() {
         return (
             <React.Fragment>
+                <div>
+                    <label>
+                        Add Comments:
+                    </label>
+                    <input type="text" name="comments" value={this.state.comments} onChange={this.updateField} />
+                </div>
+                <div>
+                    <label>
+                        Username:
+                    </label>
+                    <input type="text" name="username" value={this.state.username} onChange={this.updateField} />
+                </div>
+                <div>
+                    <label>
+                        Recipe name:
+                    </label>
+                    <input type="text" name="recipe_name" value={this.state.recipe_name} onChange={this.updateField} />
+                </div>
+                <div>
+                    <label>
+                        User id:
+                    </label>
+                    <input type="text" name="user_id" value={this.state.user_id} onChange={this.updateField} />
+                </div>
+                <div>
+                    <label>
+                        Recipe id:
+                    </label>
+                    <input type="text" name="recipe_id" value={this.state.recipe_id} onChange={this.updateField} />
+                </div>
+                <button onClick={this.state.add}>Add</button>
+
+
                 {this.renderCommentsList()}
             </React.Fragment>
 
