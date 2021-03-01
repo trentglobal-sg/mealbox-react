87644905c40312bbb738aaa38eb92f74ed7528d7
yongsannnn <yong_s@live.com>
2021-03-01 07:41:48 +0000

    Initial Commit & included AddComments.js

---
+++ mealbox/src/AddComments.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default class AddComments extends React.Component {
+    state = {}
+    render() {
+        return (
+            <React.Fragment>
+                You should see this
+            </React.Fragment>
+
+
+        )
+    }
+}
---
+++ mealbox/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AddComments from "./AddComments"
+
+function App() {
+  return (
+    <div className="App">
+    <AddComments/>
+    </div>
+  );
+}
+
+export default App;
