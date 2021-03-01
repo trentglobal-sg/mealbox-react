cfd61efaaebb2c91ca98f66618b3f00602fc3318
yongsannnn <yong_s@live.com>
2021-03-01 07:56:59 +0000

    Added axios and completed list rendering

--- mealbox/src/AddComments.js
+++ mealbox/src/AddComments.js
@@ -1,11 +1,40 @@
 import React from "react"
+import axios from "axios"
+
+const baseURL = "https://3001-bronze-barnacle-pdcp8mf3.ws-us03.gitpod.io"
+
 
 export default class AddComments extends React.Component {
-    state = {}
+    state = {
+        comments: []
+    }
+
+    // Lifecycle Import
+    async componentDidMount() {
+        let response = await axios.get(baseURL + "/comments");
+        this.setState({
+            comments: response.data
+        })
+    }
+
+    renderCommentsList = () => {
+        let list = [];
+        for (let l of this.state.comments) {
+            list.push(
+                <div key={l._id}>
+                    <h5>{l.recipe_name}</h5>
+                    <p>{l.comments}</p>
+                    <p>By: {l.username}</p>
+                </div>
+            )
+        }
+        return list
+    }
+
     render() {
         return (
             <React.Fragment>
-                You should see this
+                {this.renderCommentsList()}
             </React.Fragment>
 
 
--- mealbox/src/App.js
+++ mealbox/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import AddComments from "./AddComments"
 
+
 function App() {
   return (
     <div className="App">
-    <AddComments/>
+        <AddComments/>
     </div>
   );
 }
