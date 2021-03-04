e7f0cb2edd5e70c34269f650279c314a3ca3c51d
yongsannnn <yong_s@live.com>
2021-03-04 10:00:11 +0000

    created CreateRecipe

---
+++ mealbox/src/CreateRecipe.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default class CreateRecipe extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                
+            </React.Fragment>
+        )
+    }
+}
