ebfe1771463f9d3f78337429ed3d3f958bacabcd
yongsannnn <yong_s@live.com>
2021-03-08 05:10:16 +0000

    Styling for CreateRecipe until description

--- mealbox/src/CreateRecipe.js
+++ mealbox/src/CreateRecipe.js
@@ -19,7 +19,7 @@ export default class CreateRecipe extends React.Component {
         serving: "",
         created_by: "",
         //Resources Collection
-        img_url:"",
+        img_url: "",
 
 
 
@@ -59,7 +59,7 @@ export default class CreateRecipe extends React.Component {
             list.push(
                 <div key={l._id}>
                     <h5>{l.recipe_name}</h5>
-                    <p>"{l.description}</p>
+                    <p>{l.description}</p>
                     <p>{l.cuisine_type}</p>
                     <p>By: {l.created_by}</p>
                     <p>{(l.created_on).slice(0, 10)}</p>
@@ -122,90 +122,111 @@ export default class CreateRecipe extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <h1>Create Recipe</h1>
-                <div>
-                    <label>
-                        Your Name:
-                    </label>
-                    <input type="text" name="created_by" value={this.state.created_by} onChange={this.updateField} required />
-                </div>
-                <div>
-                    <label>
-                        Recipe Name:
-                    </label>
-                    <input type="text" name="recipe_name" value={this.state.recipe_name} onChange={this.updateField} />
-                </div>
-                <div>
-                    <label>
-                        Image URL:
-                    </label>
-                    <input type="text" name="img_url" value={this.state.img_url} onChange={this.updateField} />
-                </div>
-                <div>
-                    <label>
-                        Description:
-                    </label>
-                    <input type="text" name="description" value={this.state.description} onChange={this.updateField} />
-                </div>
-                <div>
-                    <label>
-                        Cuisine Type:
-                    </label>
-                    <select name="cuisine_type" value={this.state.cuisine_type} onChange={this.updateField}>
-                        <option>American</option>
-                        <option>Chinese</option>
-                        <option>Italian</option>
-                        <option>Japanese</option>
-                    </select>
-                </div>
-                <div>
-                    <label>
-                        Cooking Time:
-                    </label>
-                    <input type="text" name="cooking_time" value={this.state.cooking_time} onChange={this.updateField} />
-                </div>
-                <div>
-                    <label>
-                        Preparation Time:
-                    </label>
-                    <input type="text" name="preparation_time" value={this.state.preparation_time} onChange={this.updateField} />
-                </div>
-                <div>
-                    <label>
-                        Serving Size:
-                    </label>
-                    <input type="text" name="serving" value={this.state.serving} onChange={this.updateField} />
-                </div>
-                <div>
-                    <label>Difficulty :</label>
-                    <input type="radio" name="difficulty" value="Easy" onChange={this.updateField} checked={this.state.difficulty === "Easy"} /><label>Easy</label>
-                    <input type="radio" name="difficulty" value="Moderate" onChange={this.updateField} checked={this.state.difficulty === "Moderate"} /><label>Moderate</label>
-                    <input type="radio" name="difficulty" value="Hard" onChange={this.updateField} checked={this.state.difficulty === "Hard"} /><label>Hard</label>
-                </div>
-                <div>
-                    <label>Tags:</label>
-                    <input type="checkbox" name="tags" value="Slow Cook Required" onChange={this.updateTags} checked={this.state.tags.includes("Slow Cook Required")} /><label>Slow Cook Required</label>
-                    <input type="checkbox" name="tags" value="Made From Scratch" onChange={this.updateTags} checked={this.state.tags.includes("Made From Scratch")} /><label>Made From Scratch</label>
-                    <input type="checkbox" name="tags" value="Date Night Special" onChange={this.updateTags} checked={this.state.tags.includes("Date Night Special")} /><label>Date Night Special</label>
-                    <input type="checkbox" name="tags" value="5-Minutes Or Less" onChange={this.updateTags} checked={this.state.tags.includes("5-Minutes Or Less")} /><label>5-Minutes Or Less</label>
-                    <input type="checkbox" name="tags" value="Suitable For All" onChange={this.updateTags} checked={this.state.tags.includes("Suitable For All")} /><label>Suitable For All</label>
-                    <input type="checkbox" name="tags" value="Kids Favourite" onChange={this.updateTags} checked={this.state.tags.includes("Kids Favourite")} /><label>Kids Favourite</label>
-                </div>
-                <div>
-                    <label>Ingredients:</label>
-                    <div>
-                        <textarea name="ingredients" rows="2" cols="30" placeholder="Seperate each ingredients by a comma" value={this.state.ingredients} onChange={this.updateField}></textarea>
-                    </div>
-                </div>
-                <div>
-                    <label>Instructions:</label>
-                    <div>
-                        <textarea name="instructions" rows="2" cols="30" placeholder="Seperate each instructions by a comma" value={this.state.instructions} onChange={this.updateField}></textarea>
-                    </div>
-                </div>
+                <div className="container-fluid p-3">
+                    <section className="row">
+                        <h1 className="col-12">Create Recipe</h1>
+                        {/* Left Box  */}
+                        <div className="col-8 left-box">
+                            <div className="form-group">
+                                <label>
+                                    Your Name:
+                            </label>
+                                <input type="text" name="created_by" className="form-control" value={this.state.created_by} onChange={this.updateField} required />
+                            </div>
+                            <div className="form-group">
+                                <label>
+                                    Recipe Name:
+                             </label>
+                                <input type="text" name="recipe_name" className="form-control" value={this.state.recipe_name} onChange={this.updateField} />
+                            </div>
+                            <div className="form-group">
+                                <label>
+                                    Recipe Image URL:
+                        </label>
+                                <input type="text" name="img_url" className="form-control" value={this.state.img_url} onChange={this.updateField} />
+                            </div>
+                            <div>
+                                <label>
+                                    Cuisine Type:
+                                </label>
+                                <select className="form-control my-1 mr-sm-2" name="cuisine_type" value={this.state.cuisine_type} onChange={this.updateField}>
+                                    <option selected>Choose...</option>
+                                    <option>American</option>
+                                    <option>Chinese</option>
+                                    <option>Italian</option>
+                                    <option>Japanese</option>
+                                </select>
+                            </div>
+                        </div>
 
-                <button onClick={this.add}>Submit</button>
-                {/* {this.renderList()} */}
+                        {/* Right Box */}
+                        <div className="col-4 right-box">
+                            <div className="form-group">
+                                <label>
+                                    Cooking Time:
+                             </label>
+                                <input type="text" name="cooking_time" className="form-control" value={this.state.cooking_time} onChange={this.updateField} />
+                            </div>
+                            <div className="form-group">
+                                <label>
+                                    Preparation Time:
+                            </label>
+                                <input type="text" name="preparation_time" className="form-control" value={this.state.preparation_time} onChange={this.updateField} />
+                            </div>
+                            <div className="form-group">
+                                <label>
+                                    Serving Size:
+                             </label>
+                                <input type="text" name="serving" className="form-control" value={this.state.serving} onChange={this.updateField} />
+                            </div>
+                            <div>
+                                <label>Difficulty:</label>
+                                <div style={{
+                                    textAlign: "center",
+                                    marginTop: "5px"
+                                }}>
+                                    <input type="radio" name="difficulty" className="mr-1" value="Easy" onChange={this.updateField} checked={this.state.difficulty === "Easy"} /><label className="mr-2">Easy</label>
+                                    <input type="radio" name="difficulty" className="mr-1" value="Moderate" onChange={this.updateField} checked={this.state.difficulty === "Moderate"} /><label className="mr-2">Moderate</label>
+                                    <input type="radio" name="difficulty" className="mr-1" value="Hard" onChange={this.updateField} checked={this.state.difficulty === "Hard"} /><label className="mr-2">Hard</label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <section className="col-12">
+                            <div className="form-group">
+                                <label>
+                                    Description:
+                            </label>
+                                <input type="text" name="description" className="form-control" value={this.state.description} onChange={this.updateField} />
+                            </div>
+                            <div>
+                                <label>Tags:</label>
+                                <input type="checkbox" name="tags" value="Slow Cook Required" onChange={this.updateTags} checked={this.state.tags.includes("Slow Cook Required")} /><label>Slow Cook Required</label>
+                                <input type="checkbox" name="tags" value="Made From Scratch" onChange={this.updateTags} checked={this.state.tags.includes("Made From Scratch")} /><label>Made From Scratch</label>
+                                <input type="checkbox" name="tags" value="Date Night Special" onChange={this.updateTags} checked={this.state.tags.includes("Date Night Special")} /><label>Date Night Special</label>
+                                <input type="checkbox" name="tags" value="5-Minutes Or Less" onChange={this.updateTags} checked={this.state.tags.includes("5-Minutes Or Less")} /><label>5-Minutes Or Less</label>
+                                <input type="checkbox" name="tags" value="Suitable For All" onChange={this.updateTags} checked={this.state.tags.includes("Suitable For All")} /><label>Suitable For All</label>
+                                <input type="checkbox" name="tags" value="Kids Favourite" onChange={this.updateTags} checked={this.state.tags.includes("Kids Favourite")} /><label>Kids Favourite</label>
+                            </div>
+                            <div>
+                                <label>Ingredients:</label>
+                                <div>
+                                    <textarea name="ingredients" rows="2" cols="30" placeholder="Seperate each ingredients by a comma" value={this.state.ingredients} onChange={this.updateField}></textarea>
+                                </div>
+                            </div>
+                            <div>
+                                <label>Instructions:</label>
+                                <div>
+                                    <textarea name="instructions" rows="2" cols="30" placeholder="Seperate each instructions by a comma" value={this.state.instructions} onChange={this.updateField}></textarea>
+                                </div>
+                            </div>
+
+                            <button onClick={this.add}>Submit</button>
+
+                        </section>
+                        {/* {this.renderList()} */}
+                    </section>
+                </div>
             </React.Fragment>
         )
     }
