5c4f470871b22a116797983dec13601ca88bc87c
yongsannnn <yong_s@live.com>
2021-03-08 08:56:40 +0000

    Set up basic footer bar

--- mealbox/src/App.js
+++ mealbox/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 // import AddComments from "./AddComments"
 import CreateRecipe from './CreateRecipe';
 import Header from "./Header"
+import Footer from "./Footer"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Header/>
         <CreateRecipe/>
         {/* <AddComments/> */}
+        <Footer/>
     </div>
   );
 }
---
+++ mealbox/src/Footer.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+export default class Footer extends React.Component {
+    state = {
+
+    }
+
+
+    render() {
+        return (
+            <React.Fragment>
+                <footer class="container-fluid footer-wrapper">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-8 col-10 mx-auto my-auto footer-des">
+                            <div> FOOTER
+                            </div>
+                        </div>
+                        <div class="space"></div>
+                    </div>
+                </footer>
+            </React.Fragment>
+        )
+    }
+
+
+}
