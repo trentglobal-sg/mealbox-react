a85fe39eed60a2eab79025c08e9d8883f743e66f
yongsannnn <yong_s@live.com>
2021-03-09 07:36:57 +0000

    included in hero image and made sm mobile device friendly

--- mealbox/src/ViewAll.js
+++ mealbox/src/ViewAll.js
@@ -27,9 +27,11 @@ export default class ViewAll extends React.Component {
                     <div className="des-container col-12 col-md-8 mt-2">
                         <h4>{l.recipe_name}</h4>
                         <p>{l.description}</p>
-                        <p>By: {l.created_by}</p>
-                        <button>Edit</button>
-                        <button>Delete</button>
+                        <p>By: <strong>{l.created_by}</strong></p>
+                        <div className="des-buttons">
+                        <button className="btn action-buttons btn-success">Edit</button>
+                        <button className="btn action-buttons btn-danger ml-2">Delete</button>
+                        </div>
                     </div>
                 </div>
             )
@@ -40,6 +42,9 @@ export default class ViewAll extends React.Component {
     render() {
         return (
             <React.Fragment>
+                <div className="hero-img">
+                    <div className="cover-overlay"></div>
+                </div>
                 <div className="container">
                     <div className="row">
                         <h1 className="col-12" style={{
@@ -47,20 +52,6 @@ export default class ViewAll extends React.Component {
                         }}>View All
                         </h1>
 
-                        {/* <div className="box col-12 p-2">
-                            <div className="image-container col-4">
-                                <img src="https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Fettuccine-Carbonara_exps175448_SD143205B01_28_2bC_RMS.jpg" className="test-img" alt="recipe"></img>
-                            </div>
-                            <div className="des-container col-8 mt-2">
-                                <h4>Fettuccini Carbonara</h4>
-                                <p>This carbonara is a delectable, mouth watering pile of yummy goodness. I recommend a nice salad with it - that's all you will need for a complete meal.</p>
-                                <p>By: Sarah J.Pixy</p>
-                                <button>Edit</button>
-                                <button>Delete</button>
-                            </div>
-                        </div>
-                        <div className="box col-12 col-md-6 col-lg-4 p-2"></div>
-                        <div className="box col-12 col-md-6 col-lg-4 p-2"></div> */}
 
                     </div>
                     {this.renderList()}
