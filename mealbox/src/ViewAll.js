4aabad2fe7725fc2f6a3cb398ce46a7d6b87b6c2
yongsannnn <yong_s@live.com>
2021-03-10 05:50:49 +0000

    Added show all as one of the option to reset all the search field

--- mealbox/src/ViewAll.js
+++ mealbox/src/ViewAll.js
@@ -55,16 +55,18 @@ export default class ViewAll extends React.Component {
 
                     </div>
                     <div className="filter-bar">
-                        <input type="text" className="form-control my-1 mr-sm-2" name="search-field" placeholder="Search Recipe Name" />
-                        <select className="form-control cuisine-bar my-1 mr-sm-2" name="cuisine_type" value={this.state.cuisine_type}>
+                        <input type="text" className="form-control my-1 mx-sm-2" name="search-field" placeholder="Search Recipe Name" />
+                        <select className="form-control cuisine-bar my-1 mx-sm-2" name="cuisine_type" value={this.state.cuisine_type}>
                             <option defaultValue>Cuisine Type</option>
+                            <option>Show All</option>
                             <option>American</option>
                             <option>Chinese</option>
                             <option>Italian</option>
                             <option>Japanese</option>
                         </select>
-                        <select className="form-control difficulty-bar my-1 mr-sm-2" name="difficulty" value={this.state.cuisine_type} >
+                        <select className="form-control difficulty-bar my-1 mx-sm-2" name="difficulty" value={this.state.cuisine_type} >
                             <option defaultValue>Difficulty Level</option>
+                            <option>Show All</option>
                             <option>Easy</option>
                             <option>Moderate</option>
                             <option>Hard</option>
