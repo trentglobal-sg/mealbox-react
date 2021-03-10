f676eabbacd5fd6fde07f6914d807ac21cbb7975
yongsannnn <yong_s@live.com>
2021-03-10 05:47:35 +0000

    Added filter options in ViewAll.js

--- mealbox/src/ViewAll.js
+++ mealbox/src/ViewAll.js
@@ -29,8 +29,8 @@ export default class ViewAll extends React.Component {
                         <p>{l.description}</p>
                         <p>By: <strong>{l.created_by}</strong></p>
                         <div className="des-buttons">
-                        <button className="btn action-buttons btn-success">Edit</button>
-                        <button className="btn action-buttons btn-danger ml-2">Delete</button>
+                            <button className="btn action-buttons btn-success">Edit</button>
+                            <button className="btn action-buttons btn-danger ml-2">Delete</button>
                         </div>
                     </div>
                 </div>
@@ -53,6 +53,22 @@ export default class ViewAll extends React.Component {
                         </h1>
 
 
+                    </div>
+                    <div className="filter-bar">
+                        <input type="text" className="form-control my-1 mr-sm-2" name="search-field" placeholder="Search Recipe Name" />
+                        <select className="form-control cuisine-bar my-1 mr-sm-2" name="cuisine_type" value={this.state.cuisine_type}>
+                            <option defaultValue>Cuisine Type</option>
+                            <option>American</option>
+                            <option>Chinese</option>
+                            <option>Italian</option>
+                            <option>Japanese</option>
+                        </select>
+                        <select className="form-control difficulty-bar my-1 mr-sm-2" name="difficulty" value={this.state.cuisine_type} >
+                            <option defaultValue>Difficulty Level</option>
+                            <option>Easy</option>
+                            <option>Moderate</option>
+                            <option>Hard</option>
+                        </select>
                     </div>
                     {this.renderList()}
                 </div>
